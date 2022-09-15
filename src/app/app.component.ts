30c7f0e248634399738192c7f7e5598ed4f47f3b
Albagnach <alban.roux@toucantoco.com>
2022-09-15 17:01:49 +0200

    get access token when url match api name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   async copyToken() {
-    const tab = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-
-    const url = tab[0].url as string;
-
-    chrome.cookies.get({ name: "PHPSESSID", url }, function (cookie) {
-      console.log('cookie : ', cookie);
+    const tab = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
     });
 
+    const url = ('https://api-' +
+      tab[0].url?.slice(0, -1).substring(8)) as string;
+    console.log('url :', url);
+    const cookie = await chrome.cookies.get({
+      name: 'access_token',
+      url,
+    });
+    console.log('token : ', cookie?.value);
   }
 }
